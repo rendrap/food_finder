15fc02ca9826d1d17fa63cae624a84e9313366a1
rendrap <rendrapradana@gmail.com>
2017-07-07 16:32:05 +0700

    add print loop

--- lib/hai.rb
+++ lib/hai.rb
@@ -1,4 +1,12 @@
 puts "helllo"
-puts "nya"
+puts "nya\n\n"
 
 # this is comment
+
+list = %w[alpha bravo charlie delta echo]
+
+n = list.length-1
+n.times do |i|
+print "list [#{i}] = #{list[i]}\n"
+end
+
